9283fb56a9472ba7b6b06650078acc18b18a3a9d
vagrant <goodmountain37@gmail.com>
2020-10-28 05:49:07 +0000

    [fix] jpostal js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,17 @@
 //= require popper
 //= require bootstrap-sprockets
 
-$(window).ready( function() {
-  $("#user_postal_code").jpostal({
-    postcode: ["#user_postal_code"],
-    address: {
-      "#user_prefecture_code": "%3",
-      "#user_city": "%4%5",
-      "#user_street": "%6",
-      "#user_building": "%7"
-    },
+$(function() {
+  $(document).on('turbolinks:load', () => {
+    $("#user_postal_code").jpostal({
+      postcode: ["#user_postal_code"],
+      address: {
+        "#user_prefecture_code": "%3",
+        "#user_city": "%4%5",
+        "#user_street": "%6",
+        "#user_building": "%7"
+      },
+    });
   });
 });
 
@@ -51,7 +53,7 @@ $(function() {
 });
 
 $(function() {
-  $(document).ready(function() {
+  $(document).on('turbolinks:load', () => {
     $('#tags').tagit();
   });
 })
