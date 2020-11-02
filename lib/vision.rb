a8ca224357dee6e7f8ee9ae5b6b9820b18bf32ba
vagrant <goodmountain37@gmail.com>
2020-11-02 10:51:41 +0000

    [add] post_image debug logger 4

--- lib/vision.rb
+++ lib/vision.rb
@@ -9,9 +9,11 @@ module Vision
       api_url = "https://vision.googleapis.com/v1/images:annotate?key=#{ENV['GOOGLE_API_KEY']}"
 
       # 画像をbase64にエンコード
+      Rails.logger.debug "ここ1？"
       base64_image = Base64.encode64(open("#{Rails.root}/public#{image_file}").read)
 
       # APIリクエスト用のJSONパラメータ
+      Rails.logger.debug "ここ2？"
       params = {
         requests: [{
           image: {
@@ -26,6 +28,7 @@ module Vision
       }.to_json
 
       # Google Cloud Vision APIにリクエスト
+      Rails.logger.debug "ここ3？"
       uri = URI.parse(api_url)
       https = Net::HTTP.new(uri.host, uri.port)
       https.use_ssl = true
@@ -34,8 +37,10 @@ module Vision
       response = https.request(request, params)
 
       # APIレスポンス出力
-      Rails.logger.debug JSON.parse(response.body)['responses'][0]['safeSearchAnnotation']
+      Rails.logger.debug "ここ4？"
       Rails.logger.debug "#{Rails.root}/public#{image_file}"
+      Rails.logger.debug "ここ5？"
+      Rails.logger.debug JSON.parse(response.body)['responses'][0]['safeSearchAnnotation']
       result = JSON.parse(response.body)['responses'][0]['safeSearchAnnotation']
       if result.value?("LIKELY") || result.value?("VERY_LIKELY")
         return false
